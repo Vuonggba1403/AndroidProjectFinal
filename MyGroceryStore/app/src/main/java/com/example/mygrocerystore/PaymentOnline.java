bedcce734e96bdd464ef442561fc3c2e91bb3428
vuonggba1403 <vuongba140302@gmail.com>
2023-05-16 08:02:16 +0700

    Update code

---
+++ MyGroceryStore/app/src/main/java/com/example/mygrocerystore/PaymentOnline.java
@@ -0,0 +1,14 @@
+package com.example.mygrocerystore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentOnline extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_online);
+    }
+}
