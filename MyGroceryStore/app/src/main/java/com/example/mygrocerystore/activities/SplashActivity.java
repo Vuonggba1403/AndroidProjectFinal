81872cfec0f20f382b51e83873fdbfe7c9ba2e2f
vuonggba1403 <vuongba140302@gmail.com>
2023-05-18 21:46:30 +0700

    update splash

--- MyGroceryStore/app/src/main/java/com/example/mygrocerystore/activities/SplashActivity.java
+++ MyGroceryStore/app/src/main/java/com/example/mygrocerystore/activities/SplashActivity.java
@@ -3,6 +3,12 @@ package com.example.mygrocerystore.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mygrocerystore.R;
@@ -12,10 +18,32 @@ import com.google.firebase.auth.FirebaseUser;
 public class
 SplashActivity extends AppCompatActivity {
 
+    private static int SPLASH_SCREEN = 5000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //variables
+        Animation topAnim,bottomAnim;
+        ImageView image;
+        TextView logo,slogan;
+
+
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+        //animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        image = findViewById(R.id.imagesplash);
+        logo = findViewById(R.id.txtlogo);
+        slogan = findViewById(R.id.txtslogan);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
@@ -31,6 +59,6 @@ SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 2000);
+        }, SPLASH_SCREEN);
     }
 }
