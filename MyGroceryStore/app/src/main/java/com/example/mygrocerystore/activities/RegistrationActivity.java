4cd0751e0be604b2709e0d52b7a4ccb3ee6fe6e2
vuonggba1403 <vuongba140302@gmail.com>
2023-05-18 20:16:55 +0700

    hi

--- MyGroceryStore/app/src/main/java/com/example/mygrocerystore/activities/RegistrationActivity.java
+++ MyGroceryStore/app/src/main/java/com/example/mygrocerystore/activities/RegistrationActivity.java
@@ -16,10 +16,16 @@ import android.widget.Toast;
 import com.example.mygrocerystore.R;
 import com.example.mygrocerystore.models.UserModel;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistrationActivity extends AppCompatActivity {
      Button signUp;
@@ -84,27 +90,43 @@ public class RegistrationActivity extends AppCompatActivity {
         }
 
         // Create User
+        // Create User
         auth.createUserWithEmailAndPassword(userEmail, userPassword)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if ( task.isSuccessful()) {
-
+                        if (task.isSuccessful()) {
                             UserModel userModel = new UserModel(userName, userEmail, userPassword);
                             String id = task.getResult().getUser().getUid();
                             database.getReference().child("User").child(id).setValue(userModel);
+
+                            // Create "useraccount" collection and save data
+                            FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+                            Map<String, Object> userData = new HashMap<>();
+                            userData.put("name", userName);
+                            userData.put("email", userEmail);
+                            userData.put("password", userPassword);
+                            firestore.collection("useraccount").document(id).set(userData)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            progressBar.setVisibility(View.GONE);
+                                            Toast.makeText(RegistrationActivity.this, "Registration successful!", Toast.LENGTH_SHORT).show();
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            progressBar.setVisibility(View.GONE);
+                                            Toast.makeText(RegistrationActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                        } else {
                             progressBar.setVisibility(View.GONE);
-                            Toast.makeText(RegistrationActivity.this, "Registration Successful !", Toast.LENGTH_SHORT).show();
-                        }
-                         else {
-                            progressBar.setVisibility(View.GONE);
-                            Toast.makeText(RegistrationActivity.this, "Error" + task.getException(), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(RegistrationActivity.this, "Error: " + task.getException(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
-
-
     }
-
 
 }
